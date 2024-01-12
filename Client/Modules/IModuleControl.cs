63acffca3eea3edd164571b60ed5f4b71d82bab9
Ron Maylon <ron.maylon@pmi.org>
2024-01-13 00:58:49 +0800

    dynamic component load

---
+++ Client/Modules/IModuleControl.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp2.Client.Modules
+{
+    public interface IModuleControl
+    {
+
+    }
+}
---
+++ RazorClassLibrary1/Services/RazorClassLibraryService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp2.Shared;
+
+namespace RazorClassLibrary1.Services
+{
+    internal sealed class RazorClassLibraryService : IRazorClassLibraryService, IService
+    {
+        public Task<int> CountAsync()
+        {
+            var random = new Random();
+            return Task.FromResult(random.Next());
+        }
+    }
+}
