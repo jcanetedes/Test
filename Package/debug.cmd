9bb68a925d05f9e5d50d5cc090a0513fb01b4a54
Ron Maylon <ron.maylon@pmi.org>
2024-01-16 22:28:55 +0800

    added pause

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,4 +1,6 @@
-﻿XCOPY "..\Plugin1\bin\Debug\net6.0\Plugin1.dll" "..\Test2\bin\Debug\net6.0\" /Y
+﻿@ECHO ON
+XCOPY "..\Plugin1\bin\Debug\net6.0\Plugin1.dll" "..\Test2\bin\Debug\net6.0\" /Y
 XCOPY "..\Plugin1\bin\Debug\net6.0\Plugin1.pdb" "..\Test2\bin\Debug\net6.0\" /Y
 XCOPY "..\Plugin2\bin\Debug\net6.0\Plugin2.dll" "..\Test2\bin\Debug\net6.0\" /Y
 XCOPY "..\Plugin2\bin\Debug\net6.0\Plugin2.pdb" "..\Test2\bin\Debug\net6.0\" /Y
+PAUSE
